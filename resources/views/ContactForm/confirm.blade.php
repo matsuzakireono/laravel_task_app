9aba421ec1f848f878c7991737fc8cfc1e305cec
matsuzakireono <r.matsuzaki@sonicmoov.com>
2023-05-24 10:28:15 +0900

    confirm.blade.php編集

--- resources/views/ContactForm/confirm.blade.php
+++ resources/views/ContactForm/confirm.blade.php
@@ -1,7 +1,5 @@
-{{-- ダウンロードしてきたBootStrap のソースファイルを読み込む --}}
 <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-{{-- ダウンロードしてきたFontAwesome のソースファイルを読み込む --}}
-<link rel="stylesheet" href="{{ asset('css/all.css'); }}">
+<link rel="stylesheet" href="{{ asset('css/all.css')}}">
 <h1>resources/views/ContactForm/confirm.blade.php</h1>
 <p>お問い合わせ内容確認</p>
 <ul>
@@ -9,15 +7,6 @@
         お名前：
         <p>{{$request->name}}</p>
     </li>
-   <!-- <form action="" method="POST">
-        {{-- type="hidden" で、画面非表示の状態で入力内容を保持しておく --}}
-        <input type="hidden" name="name" value="{{$request->name}}">
-        <div>
-            {{-- 戻るボタンにback というname属性を持たせておき、ボタンが押されたかどうかを判定できるようにする --}}
-            <input type="submit" name="back" value="戻る">
-        </div>
-        @csrf
-    </form>-->
     <button class="btn btn-primary" type="button" onClick="history.back()">
         <i class="fa-solid fa-caret-left"></i>
         戻る
