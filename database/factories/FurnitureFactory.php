5857b1300e4d0b79271d73ce005712b45a45dcd4
matsuzakireono <r.matsuzaki@sonicmoov.com>
2023-05-25 18:32:43 +0900

    ソート機能まで完成

--- database/factories/FurnitureFactory.php
+++ database/factories/FurnitureFactory.php
@@ -26,7 +26,7 @@ class FurnitureFactory extends Factory
             'price' => $this->faker->numberBetween($min, $max),
             'prefecture' => $this->faker->prefecture, // 都道府県に修正
             'address' => $this->faker->address,
-            'image' => 'https://picsum.photos/400/300',
+            'image' => 'https://picsum.photos/400/300?random=',
             'review' => $this->faker->randomFloat(1, 1, 5),
             'is_visible' => $this->faker->boolean,
             'created_at' => $this->faker->dateTimeBetween('-10 year', 'now')->format('Y/m/d H:i:s'),
--- resources/views/furnitures/index.blade.php
+++ resources/views/furnitures/index.blade.php
@@ -3,6 +3,9 @@
 <!-- 検索ボックス -->
 <form action="{{ route('furnitures.index') }}" method="GET">
     <div>
+        <label for="prefacture">地域:</label>
+    </div>
+    <div>
         <label for="sort">並び替え:</label>
         <select name="sort" id="sort">
             <option value="newest" {{ request('sort') === 'newest' ? 'selected' : '' }}>新着順</option>
@@ -19,6 +22,8 @@
                 <th>名前</th>
                 <th>価格(円)</th>
                 <th>都道府県</th>
+                <th>画像</th>
+                <th>評価</th>
                 <th>発売日</th>
             </tr>
         </thead>
@@ -28,6 +33,8 @@
                         <td>{{ $furniture->name }}</td>
                         <td>{{ $furniture->price }}</td>
                         <td>{{ $furniture->prefecture }}</td>
+                        <td><img src="{{ $furniture->image }}+{{$furniture->id}}" alt="商品画像"></td>
+                        <td>{{ $furniture->review}}</td>
                         <td>{{ $furniture->created_at }}</td>
                     </tr>
             @endforeach
