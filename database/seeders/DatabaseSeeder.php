3f920e80900f355df0402ce06983b09f62b379ea
matsuzakireono <r.matsuzaki@sonicmoov.com>
2023-05-26 15:21:08 +0900

    team coach player

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,10 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(
-            [ContactsSeeder::class]
+            [
+                ContactsSeeder::class,
+                InitializeCoachesAndTeamsSeeder::class
+            ],
         );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\YoutubeController;
 use App\Http\Controllers\SampleController;
 use App\Http\Controllers\FurnitureController;
+use App\Models\Coach;
+use App\Models\Team;
 
 
 Route::get('/sample', [SampleController::class, 'index'])->name('sample.index');
@@ -41,3 +43,42 @@ Route::middleware('auth')->group(function () {
 // Route::resource('funitures', FurnitureController::class);
 
 require __DIR__ . '/auth.php';
+
+/* Coachのデータを一覧表示する
+ * (表示したいだけなので、Controllerを作らずルータ内で処理する)
+ */
+Route::get('/coach', function () {
+    /* Coach モデルを通じて、coaches テーブルの内容をすべて取得 */
+    $all_coaches = Coach::all();
+    foreach ($all_coaches as $coach) {
+        /* $coach->teamで、関連付けされたteams テーブルのレコードの内容を取得できる */
+        print("<p>監督名： {$coach->name} (担当チーム名： {$coach->team->name})</p>");
+    }
+});
+
+/* Teamのデータを一覧表示する
+ * (表示したいだけなので、Controllerを作らずルータ内で処理する)
+ */
+Route::get('/team', function () {
+    /* Team モデルを通じて、teams テーブルのデータをすべて取得 */
+    $all_teams = Team::all();
+    foreach ($all_teams as $team) {
+        /* nullの場合があるので、ifでチェック */
+        if ($team->coach != null) {
+            $coach = $team->coach->name;
+        } else {
+            $coach = '';
+        }
+        print("<h2>チーム名： {$team->name} (監督：{$coach}) </h2>");
+        print("<p>所属プレイヤー</p>");
+        print('<ul>');
+        /* $team->playersで、関連付けされたteams テーブルのレコードの内容を取得できる
+             * Team モデルとPlayer モデルのリレーションは一対多(hasMany)
+             * 複数データが取得されるため、foreachでループしてひとつずつ処理する
+             */
+        foreach ($team->players as $player) {
+            print("<li>{$player->name}</li>");
+        }
+        print('</ul>');
+    }
+});
